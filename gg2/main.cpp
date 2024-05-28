f784490e7d59125d39049761b97180de6bb2c80b
MiNeBudemRabami <smileaschelsea@gmail.com>
2024-05-28 12:23:54 +0300

    answer printed on window

--- gg2/main.cpp
+++ gg2/main.cpp
@@ -1,62 +1,123 @@
 #include <iostream>
-#include <vector>
 #include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <math.h>
-#include <algorithm> 
+#include <string>
+
+#include "Textbox.h"
+#include "Button.h"
 
 
-using namespace std;
-
-sf::RenderWindow window(sf::VideoMode(800, 600), "padavan");
-sf::Font font;
-
-
-void draw_text()
+int padavan(int n)
 {
-	sf::Text text;
-	text.setFillColor(sf::Color::White);
-	text.setPosition(100, 100);
-	text.setFont(font);
-	text.setString("Hello world");
-	window.draw(text);
+	int pPrevPrev = 1, pPrev = 1, pCurr = 1, pNext = 1;
+
+	for (int i = 3; i <= n; i++)
+	{
+		pNext = pPrevPrev + pPrev;
+		pPrevPrev = pPrev;
+		pPrev = pCurr;
+		pCurr = pNext;
+	}
+
+	return pNext;
 }
 
-void redraw_frame()
+int main()
 {
-	window.clear(sf::Color::Black);
-	window.display();
-}
+	sf::RenderWindow window;
 
+	float window_x = 800;
+	float window_y = 600;
 
-int main() ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-{
+	window.create(sf::VideoMode(window_x, window_y), "padavan");
+
+	window.setKeyRepeatEnabled(true);
+
+	sf::Font font;
 	if (!font.loadFromFile("resources\\sansation.ttf"))
 	{
-		return 0;
+		std::cout << "Font not found!\n";
 	}
 
+	Textbox text1(14, sf::Color::White, true);
+	text1.setPosition({ 100, 100 });
+	text1.setLimit(true, 2);
+	text1.setFont(font);
+
+	Button btn1("Enter", { 200, 100 }, 30, sf::Color::Green, sf::Color::Black);
+	btn1.setFont(font);
+	btn1.setPosition({ 100, 300 });
+
+	sf::Text answer;
+	answer.setFont(font);
+	answer.setString("");
+	answer.setCharacterSize(16);
+	answer.setFillColor(sf::Color::Red);
+	answer.setStyle(sf::Text::Bold | sf::Text::Underlined);
+	answer.setPosition({100,200});
 	
-	sf::Style::Default;
-	window.clear(sf::Color::White);
-	redraw_frame();
 
-	int j = 0;
+	while (window.isOpen()) {
 
-	while (window.isOpen())
-	{
-		sf::Event event;
-		while (window.pollEvent(event))
+		sf::Event Event;
+
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Return)) {
+			text1.setSelected(true);
+		}
+		else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+			text1.setSelected(false);
+		}
+
+
+
+		while (window.pollEvent(Event)) 
 		{
-			if (event.type == sf::Event::Closed)
+			switch (Event.type)
 			{
+
+			case sf::Event::Closed:
 				window.close();
-			}
-			if (event.type == sf::Event::KeyPressed)
-			{
-				redraw_frame();
-							
+
+			case sf::Event::TextEntered:
+				text1.typedOn(Event);
+
+
+			case sf::Event::MouseMoved:
+
+				if (btn1.isMouseOver(window))
+				{
+					btn1.setBackColor(sf::Color::Magenta);
+				}
+				else {
+					btn1.setBackColor(sf::Color::Green);
+				}
+				break;
+
+			case sf::Event::MouseButtonPressed:
+
+				if (btn1.isMouseOver(window))
+				{
+
+
+					std::string input_string = text1.getText();
+					
+					if (input_string.length() == 0)
+					{
+						input_string = "0";
+					}
+
+					answer.setString(std::to_string(padavan(std::stoi(input_string) - 1)));
+
+					std::cout << padavan(std::stoi(input_string) - 1) << "\n";
+
+				}
 			}
 		}
+
+		window.clear();
+		text1.drawTo(window);
+		btn1.drawTo(window);
+		window.draw(answer);
+		window.display();
 	}
+
 }
